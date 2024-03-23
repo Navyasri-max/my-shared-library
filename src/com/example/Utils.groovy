32e993070be2b8bc87fd1ed7daec805d8d3d02b4
root <root@ip-172-31-53-219.ec2.internal>
2024-03-23 12:48:34 +0000

    update

--- src/com/example/Utils.groovy
+++ src/com/example/Utils.groovy
@@ -2,7 +2,7 @@ def call(name) {
        pipeline {
            agent any
            stages {
-               stage('print_name') {
+               stage('print_name3') {
                    steps {
                        echo "Hello $name"
                        }
